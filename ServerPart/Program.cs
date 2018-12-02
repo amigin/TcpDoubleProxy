69e6b3436a76721320da3372f66b93e7c0d74fbf
amigin <33rkjgerhjg51>
2018-12-02 02:17:15 +0200

    made ports for Server side adjustable via console arguments

--- ServerPart/Program.cs
+++ ServerPart/Program.cs
@@ -7,23 +7,30 @@ namespace ServerPart
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-            const int clientListenPort = 3389;
-            const int techListenPort = 5666;
-            
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Please specify Service Port and Tech Port. Ex: dotnet run 3389 5666");
+                return;
+            }
+
+            var clientListenPort = int.Parse(args[0]);
+
+            var techListenPort = int.Parse(args[1]);
+
             var serviceListenSocket = new ServiceListenSocket(clientListenPort);
             var technicalListenSocket = new TechnicalListenSocket(techListenPort);
 
-            serviceListenSocket.BindToSocket(technicalListenSocket);            
+            serviceListenSocket.BindToSocket(technicalListenSocket);
             technicalListenSocket.BindToSocket(serviceListenSocket);
-            
+
             technicalListenSocket.Start();
             serviceListenSocket.Start();
-            
-            Console.WriteLine("Client Socket started: "+clientListenPort);
-            Console.WriteLine("Tech Socket started: "+techListenPort);
+
+            Console.WriteLine("Client Socket started: " + clientListenPort);
+            Console.WriteLine("Tech Socket started: " + techListenPort);
 
             while (true)
             {
